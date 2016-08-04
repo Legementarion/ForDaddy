fc0ec92410d4ffab2ec7c0f1382559d741fb2d8a
Legementario <mr.re66it@gmail.com>
2016-08-04 17:09:02 +0300

    Initial commit

---
+++ app/src/main/java/com/lego/fordaddy/logic/DrawableProccess.java
@@ -0,0 +1,8 @@
+package com.lego.fordaddy.logic;
+
+/**
+ * @author Lego on 04.08.2016.
+ */
+
+public class DrawableProccess {
+}
---
+++ app/src/main/java/com/lego/fordaddy/utils/Const.java
@@ -0,0 +1,8 @@
+package com.lego.fordaddy.utils;
+
+/**
+ * @author Lego on 04.08.2016.
+ */
+
+public class Const {
+}
